97295ed54bbac28f18abdb9fc31b10ce667cb784
Family <family@debian-BULLSEYE-live-builder-AMD64>
2023-01-04 16:50:57 -0300

    v1.1

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ import { config as dotenv } from 'dotenv'
 dotenv()
 
 export const config = {
-  url: process.env.URL
+  url: process.env.URL,
+  port: process.env.PORT
 } 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
+import {config} from './config'
 import app from './app'
 
-app.listen(3000)
-console.log('Server running on port 3000')
+app.listen(config.port)
+console.log(`Server running on port ${config.port}`)
